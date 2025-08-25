83bc4d7d9648e3c9b076e6aeb26e196109308350
yuda-lyu <firsemisphere@gmail.com>
2025-08-25 11:23:40 +0800

    modify: update pks

--- toolg/gDistRollup.mjs
+++ toolg/gDistRollup.mjs
@@ -15,12 +15,50 @@ rollupFiles({
         return 'w-dwdata-tweqmp'
     },
     globals: {
+
         'path': 'path',
         'fs': 'fs',
+        'url': 'url',
+        'pino': 'pino',
+        'events': 'events',
+        'chokidar': 'chokidar',
+
+        'net': 'net',
+        'util': 'util',
+        'stream': 'stream',
+        'stream-combiner': 'stream-combiner',
+        'ftp-response-parser': 'ftp-response-parser',
+        'parse-listing': 'parse-listing',
+        'once': 'once',
+        'unorm': 'unorm',
+        'debug': 'debug',
+        'ssh2-sftp-client': 'ssh2-sftp-client',
+
+        'xxhash-wasm': 'xxhash-wasm',
+
     },
     external: [
+
         'path',
         'fs',
+        'url',
+        'pino',
+        'events',
+        'chokidar',
+
+        'net',
+        'util',
+        'stream',
+        'stream-combiner',
+        'ftp-response-parser',
+        'parse-listing',
+        'once',
+        'unorm',
+        'debug',
+        'ssh2-sftp-client',
+
+        'xxhash-wasm',
+
     ],
 })
 
